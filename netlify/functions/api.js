970618a577417724ff7b3c3dd0a21f9d6c917214
banhaa244-commits <banhaa244@gmail.com>
2026-07-06 23:44:09 -0300

    fix: export excel corrompido no Netlify - binaryMimeTypes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,7 +4,11 @@ const app = require('../../app');
 const handler = serverless(app, {
   request: (req, event, context) => {
     req.context = context;
-  }
+  },
+  binaryMimeTypes: [
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8'
+  ]
 });
 
 exports.handler = async (event, context) => {
